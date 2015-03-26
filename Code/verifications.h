4380f3fdfb7178880f1213e5aea8c82c521cfe49
Maxime Pineau <maxime.nicolas2@hotmail.fr>
2015-03-26 19:56:21 +0100

    erreur inconnue

---
+++ Code/verifications.h
@@ -0,0 +1,12 @@
+#ifndef _verifications
+	#define _verifications
+
+
+
+int stringToPositiveInteger( const char* string, int* nombre );
+int copierFichier( char* source, char* destination );
+
+void ajouterRepertoireCourant( char* chemin, char nouveauChemin[] );
+int verifierLibrairies( char* cheminLibrairie_1, char* cheminLibrairie_2, char* librairie_1, char* librairie_2, int* copieEffectuee ) ;
+
+#endif
