d7e6f0161c507e83dec4b617f0a716a4950b3882
Maxime Pineau <maxime.nicolas2@hotmail.fr>
2015-04-02 17:13:48 +0200

    raffinement intermédiaire + fonction calcullerCoupPossibles non fini

--- Code/fonctionsBot.c
+++ Code/fonctionsBot.c
@@ -7,12 +7,6 @@
 
 
 
-
-
-
-
-
-
 // rend les dés sous forme d'une liste chainée
 ListeChainee* getDices( const unsigned char dices[2] ) {
 
@@ -20,19 +14,12 @@ ListeChainee* getDices( const unsigned char dices[2] ) {
 
 	// on a fait un double (exemple : 1-1 , 2-2 , ... 6-6 )
 	if( dices[0] == dices[1] ) {
-		//*nbDeplacements = 4;
 		int i;
 		for( i = 0; i < 4; i++ ) ajouterElementFin( liste, dices[0] );
 	}
 	else {
 		ajouterElementFin( liste, dices[0] );
 		ajouterElementFin( liste, dices[1] );
-		
-		/*
-		*nbDeplacements = 2;
-		deplacements[0] = dices[0];
-		deplacements[1] = dices[1];
-		*/
 	}
 
 	return liste;
@@ -42,43 +29,49 @@ ListeChainee* getDices( const unsigned char dices[2] ) {
 
 
 
-// crée un nouveau mouvement
-// retourne NULL si les 
-// depart et arrivée entre 0 et 25
-// 0 : représente la barre
-// 25 : représente la sortie
-// autre : représente les cases du plateau
-// pre : 0 <= depart, arrivee <= 25
-SMove creerMouvement( int depart, int arrivee ) {
-
-	SMove mouvement;
-	mouvement.src_point = depart;
-	mouvement.dest_point = arrivee;
-
-	return mouvement;
+/* retourne la case à la position i
+		convention :
+		- 0 : la barre (gamestate -> bar[macouleur] )
+		- 1 à 24 : le plateau 
+		- 25 : la sortie (out)
+*/
+Square getCaseReelle( SGameState* gameState, Player maCouleur, int i ) {
+	
+	Square laCase;
+	
+	if( i != 0 ) laCase = gameState -> board[ i - 1 ]; 		// cas particulier : la barre est représentée par 0
+	else if( i == 25 ) {
+		laCase.owner = maCouleur;
+		laCase.nbDames = gameState -> out[ maCouleur ];
+	}
+	else {		
+		laCase.owner = maCouleur;
+		laCase.nbDames = gameState -> bar[ maCouleur ];
+	}
+	
+	return laCase;
 }
 
 
 
 
-// vrai si pion à la case depart peut etre depalcer de nbCases
-/*
-	convention :
-	- 0 : la barre (gamestate -> bar[macouleur] )
-	- 1 à 24 : le plateau 
-	- 25 : la sortie (out)
-*/
+
+
+
+// retourne vrai si le joueur désigné par sa couleur possède des pions sur la barre (pions à remettre en jeu)
+int possedeDesPionsSurLaBarre( SGameState* gameState, Player maCouleur ) {
+	return gameState -> bar[ maCouleur ];
+}
+
+
+
+//	retourne vrai si un pion à la case depart peut etre déplacer de nbCases
 int peutDeplacerUnPion( SGameState* gameState, Player maCouleur, int depart, int nbCases ) {
 
 	if( depart < 0 || depart > 24 ) return 0;		// deplacement impossible, depassement de borne
 	if( nbCases < 0 ) return 0;
 
-	Square caseDepart;
-	if( depart == 0 ) {		// on est sur la barre
-		caseDepart.owner = maCouleur;
-		caseDepart.nbDames = gameState -> bar[maCouleur];
-	}
-	else caseDepart = gameState -> board[ depart - 1 ];
+	Square caseDepart = getCaseReelle( gameState, maCouleur, depart );
 
 	if( caseDepart.nbDames <= 0 ) return 0;			// pas de pions à déplacer sur la case de départ
 	if( caseDepart.owner != maCouleur ) return 0;	// les pions ne sont pas à moi
@@ -91,13 +84,8 @@ int peutDeplacerUnPion( SGameState* gameState, Player maCouleur, int depart, int
 	
 	if( arrivee > 25 || arrivee < 0 ) arrivee = 25;
 
-	Square caseArrivee;
-	if( arrivee == 25 ) {
-		caseArrivee.owner = maCouleur;
-		caseArrivee.nbDames = gameState -> out[ maCouleur ];
-	}
-	else caseArrivee = gameState -> board[ arrivee - 1 ];
-
+	Square caseArrivee = getCaseReelle( gameState, maCouleur, arrivee );
+	
 	if( caseArrivee.owner != maCouleur && caseArrivee.nbDames > 1 ) return 0;	// il y a plus d'un pion adverse sur la case d'arrivee
 	
 	if( arrivee == 25 ) {	// si on veut sortir nos pions
@@ -118,127 +106,80 @@ int peutDeplacerUnPion( SGameState* gameState, Player maCouleur, int depart, int
 }
 
 
-// ATTENTION ===> NE VERIFIE PAS SI ON PEUT DEPLACER LE PION !!!!!!
-// pre : appel de la fonction : peutBougerPion
-SMove deplacerUnPion( SGameState* gameState, Player maCouleur, int depart, int nbCases ) {
-
-	// le noir se déplace dans le sens normal, le blanc dans le sens inverse
-	int arrivee;
-	if( maCouleur == BLACK ) arrivee = depart + nbCases;
-	else if( depart == 0 ) arrivee = 24 - nbCases;		// je suis le WHITE
-	else arrivee = depart - nbCases;
-
-	if( arrivee > 25 || arrivee < 0 ) arrivee = 25;
 
 
-	SMove mouvement = creerMouvement( depart, arrivee );	
 
+/* déplace un pion sur le plateau du jeu, retourne le nouvel état du jeu.
+*
+* Attention : ne vérifie pas si l'on peut déplacer le pion, il faut pour cela appeler une autre fonction AVANT celle ci
+*
+*/
+SGameState deplacerUnPion( SGameState gameState, Player maCouleur, SMove mouvement ) {
+
+	int depart = mouvement.src_point;
+	int arrivee = mouvement.dest_point;
 
 	// on bouge le pion du plateau
-
-	gameState -> board[depart].nbDames--;
-	if( ! gameState -> board[depart].nbDames ) { 	// == 0
-		gameState -> board[depart].owner = NOBODY;
+	gameState.board[depart].nbDames--;
+	if( ! gameState.board[depart].nbDames ) { 	// == 0
+		gameState.board[depart].owner = NOBODY;
 	}
 
 	// on ne vérifie pas si il y a un adversaire ici	
-	gameState -> board[arrivee].nbDames++;
-	gameState -> board[arrivee].owner = maCouleur;
+	gameState.board[arrivee].nbDames++;
+	gameState.board[arrivee].owner = maCouleur;
+
+	return gameState;
+}
+
+
+
+
+
+
+
+/*	crée un nouveau mouvement du point 'depart' au point 'arrivee'
+*	
+*	0 <= depart, arriveee <= 25
+*/
+SMove creerMouvement( int depart, int arrivee ) {
+
+	SMove mouvement;
+	mouvement.src_point = depart;
+	mouvement.dest_point = arrivee;
 
 	return mouvement;
 }
 
 
+/*
+*	Crée un mouvement en fonction :
+*		- de la couleur du joueur (pour le sens de déplacement)
+*		- de la case de départ
+*		- du nombre de cases à parcourir lors du déplacement
+*
+* précondition : 	0 <= depart <= 25
+*					nbCases > 0
+*/
+SMove creerMouvementJoueur( Player maCouleur, int depart, int nbCases ) {
 
-int possedeDesPionsSurLaBarre( SGameState* gameState, Player maCouleur ) {
-	return gameState -> bar[ maCouleur ];
+	int arrivee;
+	if( maCouleur == BLACK ) arrivee = depart + nbCases;
+	else if( depart == 0 ) arrivee = 25 - nbCases;		// je suis le WHITE
+	else arrivee = depart - nbCases;
+
+	return creerMouvement( depart, arrivee );
 }
 
 
 
-// retourne la case à la position i
-Square getCaseActuelle( SGameState gameState, Player maCouleur, int i ) {
-    
-    Square laCase;
-    
-    if( i != 0 ) laCase = gameState.board[ i - 1 ]; 		// cas particulier : la barre est représentée par 0
-	else {		
-		laCase.owner = maCouleur;
-		laCase.nbDames = gameState.bar[ maCouleur ];
-	}
-	
-	return laCase;
-}
 
-
-
-void calculerMouvements( SGameState gameState, Player maCouleur, ListeChainee* dices, SMove mouvements[4], int* nbMouvements ) {
-
-	Square laCase;
-	Cellule* leDe;
-	Cellule* tmp;
-	unsigned char valeurDe;
-
-
-	// pour chaque case du plateau
-	int i;
-	for( i = 0; i < 25; i++ ) {
-
-        laCase = getCaseActuelle( gameState, maCouleur, i );
-
-		if( gameState.bar[ maCouleur ] && i != 0 ) break;	// il y a des pions sur la barre, je n'ai pas le droit de deplacer autre chose
-
-		if( laCase.owner != maCouleur || laCase.nbDames <= 0 ) continue;	// je n'ai pas de pions sur cette case
-
-
-		// pour chaque dé de la liste
-		leDe = getPremierElement(dices);
-		while( leDe ) {
-
-			valeurDe = getDonnee(leDe);
-
-			// si je ne peux pas déplacer un pion avec ce dé
-			// j'essaie avec un autre dé
-			if( peutDeplacerUnPion( &gameState, maCouleur, i, valeurDe) ) {
-
-				detruireCellule( dices, leDe );		// on retire le dé de la liste (car on l'a utilisé)
-
-
-				// si j'ai encore des dés à jouer
-				if( ! listeEstVide(dices) ) {	
-					calculerMouvements( gameState, maCouleur, dices, mouvements, nbMouvements );
-				
-					// créée un couple entre le mouvement actuel et chaque mouvement venant d'être généré
-					/*
-					
-					    PARTIE NON FINIE !!! 
-					
-					*/
-					
-					
-				}
-
-
-				leDe = ajouterElementDebut( dices, valeurDe ); 	// on remet le dé dans la liste
-
-			}
-
-			leDe = getCelluleSuivante(leDe);
-		
-		}
-
-
-	}
-
-}
-
-
-
+// initialise un coup vide (pas de mouvements)
 void initialiserCoup( Coup* coup ) {
 	coup -> nbMouvements = 0; 
 }
 
-void ajouterMouvementAuCoup( Coup* coup, Mouvement mouvement ) {
+void ajouterMouvementAuCoup( Coup* coup, SMove mouvement ) {
 	coup -> mouvements[ coup -> nbMouvements ] = mouvement;
 	coup -> nbMouvements++; 
 }
@@ -246,22 +187,25 @@ void ajouterMouvementAuCoup( Coup* coup, Mouvement mouvement ) {
 
 
 
-void calculerCoupsPossibles( SGameState gameState, Player maCouleur, ListeChainee* dices, Coup coups[], int nbCoupsPossibles ) {
-    
-    Square laCase;
-    Cellule* leDe;
-    unsigned char valeurDe;
-    SMove mouvement;
 
-    *nbCoupsPossibles = 0;
-    SGameState newGameState;
+void calculerCoupsPossibles( SGameState* gameState, Player maCouleur, ListeChainee* dices, Coup coups[], int* nbCoupsPossibles ) {
+	
+	Square laCase;
+	Cellule* leDe;
+	unsigned char valeurDe;
 
-    int i;
-    for( i = 0; i < 25; i++ ) {     // pour chaque cases du tableau
-        
-        laCase = getCaseActuelle( gameState, maCouleur, i );
+	Coup coup;
+	SMove mouvement;
+	SGameState newGameState;
 
-        if( possedeDesPionsSurLaBarre( &gameState, maCouleur ) && i != 0 ) break;	// il y a des pions sur la barre, je n'ai pas le droit de deplacer autre chose
+	*nbCoupsPossibles = 0;
+
+	int i;
+	for( i = 0; i < 25; i++ ) {	 // pour chaque cases du tableau
+		
+		laCase = getCaseReelle( gameState, maCouleur, i );
+
+		if( possedeDesPionsSurLaBarre( gameState, maCouleur ) && i != 0 ) break;	// il y a des pions sur la barre, je n'ai pas le droit de deplacer autre chose
 		if( laCase.owner != maCouleur || laCase.nbDames <= 0 ) continue;	// je n'ai pas de pions sur cette case
 
 		leDe = getPremierElement(dices);
@@ -270,33 +214,43 @@ void calculerCoupsPossibles( SGameState gameState, Player maCouleur, ListeChaine
 			valeurDe = getDonnee( leDe );
 			if( peutDeplacerUnPion( gameState, maCouleur, i, valeurDe ) ) {
 
-				mouvement = creerMouvement( i, valeurDe ); 		// pas ça ------------------------->>> !!!!!!!!!!!!!!
+				mouvement = creerMouvementJoueur( maCouleur, i, valeurDe );
+				newGameState = deplacerUnPion( *gameState, maCouleur, mouvement );
 
-				coups[ *nbCoupsPossibles ].mouvements[0] = NULL;
-				coups[ *nbCoupsPossibles ].nbMouvements = 1;
+				coup = coups[ *nbCoupsPossibles ];
+				initialiserCoup( &coup );
+				ajouterMouvementAuCoup( &coup, mouvement );
 
 
-				detruireCellule(leDe);
-
-				if( ! listeEstVide(leDe) ) {
+				detruireCellule( dices, leDe );
+				if( ! listeEstVide(dices) ) {
 
 					// pas bon non plus
-					//calculerCoupsPossibles( gameState, maCouleur, dices, coups, nbCoupsPossibles );
+					//calculerCoupsPossibles( newGameState, maCouleur, dices, coups, nbCoupsPossibles );
+					/*
+					*
+					*	NON FINIE !!!!!!!!!!!!
+					*
+					*/
+
 
 				}
+				ajouterElementDebut( dices, valeurDe );
+				
+
 
 				*nbCoupsPossibles++;
-
-
-				ajouterElementDebut( dices, valeurDe );
 			}
 
 			leDe = getCelluleSuivante(leDe);
 		}
 
 
-    }
-    
+	}
+	
 }
+
+
+
 
 
--- Code/fonctionsBot.h
+++ Code/fonctionsBot.h
@@ -1,5 +1,5 @@
 #ifndef _fonctionsBot
-    #define _fonctionsBot
+	#define _fonctionsBot
 
 
 #include "backgammon.h"
@@ -8,13 +8,13 @@
 
 // signature des fonctions communes
 struct Bot {
-    
-    char nom[50];
-    Player maCouleur;
-    unsigned int scoreCible;    // ??
-    
-    SGameState gameState;       // ??
-    
+	
+	char nom[50];
+	Player maCouleur;
+	unsigned int scoreCible;	// ??
+	
+	SGameState gameState;	   // ??
+	
 };
 typedef struct Bot Bot;
 
@@ -23,9 +23,9 @@ typedef struct Bot Bot;
 
 // combinaison de mouvements
 struct Coup {
-    
-    SMove mouvements[4];
-    int nbMouvements;
+	
+	SMove mouvements[4];
+	int nbMouvements;
 
 };
 typedef struct Coup Coup;
@@ -34,13 +34,8 @@ typedef struct Coup Coup;
 
 ListeChainee* getDices( const unsigned char dices[2] );
 
-SMove creerMouvement( int depart, int arrivee );
 
-Square getCaseActuelle( SGameState gameState, Player maCouleur, int i );
-int peutDeplacerUnPion( SGameState* gameState, Player maCouleur, int depart, int nbCases ) ;
-SMove deplacerUnPion( SGameState* gameState, Player maCouleur, int depart, int nbCases );       // a modifier peut être
-int possedeDesPionsSurLaBarre( SGameState* gameState, Player maCouleur );
 
-void calculerMouvements( SGameState gameState, Player maCouleur, ListeChainee* dices, SMove mouvements[4], int* nbMouvements );
-    
+
+
 #endif
