85acb37ec09d8e9e86d4dfa4660910cc242ac389
managarmr <paprika.dono@gmail.com>
2015-04-09 16:44:49 +0000

    comment

--- Code/IA/ListeChainee.c
+++ Code/IA/ListeChainee.c
@@ -1,9 +1,6 @@
 /*
-
 	Fichier contenant toutes les fonctions et structures concernant la liste chainée
-
-	Cette liste est utilisée pour stocké des structures Coup
-
+	Cette liste est utilisée pour stocker des structures Coup
 */
 
 #include "ListeChainee.h"
@@ -13,13 +10,22 @@
 #include <stdio.h>
 
 
-
+/** Définition de la structure Cellule
+ * @attribut valeur : la valeur de la cellule
+ * @attribut suivant : la cellule suivante
+ * @attribut precedent : la cellule précédente
+ * */
 struct Cellule {
 	Donnee valeur;
 	struct Cellule* suivant;
 	struct Cellule* precedent;
 };
 
+/** Définition de la structure correspondant aux listes chaînées
+ * @attribut nbElements : le nombre d'éléments dans la liste
+ * @attribut premier : le premier élément de la liste
+ * @attribut dernier : le dernier élément de la liste
+ * */
 struct ListeChainee {
 	int nbElements;
 	struct Cellule* premier;
@@ -27,30 +33,40 @@ struct ListeChainee {
 };
 
 
-
 /* operations sur les cellules */
 
-// retourne un pointeur vers la cellule suivante de la cellule passée en paramètre par pointeur (cette valeur est à NULL si il n'y a pas de suivant)
+
+/** Retourne un pointeur vers la cellule suivante
+ * @param cellule : la cellule actuelle
+ * @return : un pointeur de cellule ou NULL s'il n'y a pas de cellule suivante
+ * */
 Cellule* getCelluleSuivante( Cellule* cellule ) {
 	return cellule -> suivant;
 }
 
-// retourne un pointeur vers la cellule précédente de la cellule passée en paramètre par pointeur (cette valeur est à NULL si il n'y a pas de précédent)
+/** Retourne un pointeur vers la cellule précédente
+ * @param cellule : la cellule actuelle
+ * @return : un pointeur de cellule ou NULL s'il n'y a pas de cellule précédente
+ * */
 Cellule* getCellulePrecedente( Cellule* cellule ) {
 	return cellule -> precedent;
 }
 
-// retourne la Donnée stockée dans la cellule désignée par pointeur
+/** Retourne la valeur d'une cellule
+ * @param cellule : la cellule actuelle
+ * @return : la donnée
+ * */
 Donnee getDonnee( Cellule* cellule ) {
 	return cellule -> valeur;
 }
 
 
-
-
 /* operations dur la liste */
 
-// crée et initialise une liste chainée vide, la retourne par pointeur
+
+/** Créer et initialiser une liste
+ * @return : une liste vide
+ * */
 ListeChainee* creerListeChainee() {
 	ListeChainee* liste = (ListeChainee*) malloc( sizeof(ListeChainee) );
 	
@@ -61,27 +77,29 @@ ListeChainee* creerListeChainee() {
 	return liste;
 }
  
-
-// renvoie vrai si la liste chainée est vide, faux sinon
+/** Vérifie si une liste est vide
+ * @param liste : la liste à tester
+ * @return : un booléen (0 = faux, le reste = vrai)
+ * */
 int listeEstVide( ListeChainee* liste ) {
 	// si il n'y a pas de premier, ou si il n'y a pas de dernier, ou si nbElements == 0
 	return liste -> nbElements == 0;
 }
 
-
-
-// ajoute un élément en début de liste
+/** Ajoute un élément en début de liste
+ * @param liste : la liste
+ * @param elem : l'élément
+ * @return : un pointeur de cellule
+ * */
 Cellule* ajouterElementDebut( ListeChainee* liste, Donnee elem ) {
-
 	Cellule* p_cellule = (Cellule*) malloc( sizeof(Cellule) );
 
-	// on défini la cellule suivante de la nouvele cellule comme étant la premiere de la liste
+	// on définit la cellule suivante de la nouvele cellule comme étant la premiere de la liste
 	p_cellule -> valeur = elem;
 	p_cellule -> suivant = liste -> premier;
 	p_cellule -> precedent = NULL;
 
-	// si la liste était vide
-	// si elle n'avait donc pas de dernier
+	// si la liste était vide, elle n'a pas de dernier
 	if( ! liste -> dernier ) {
 		liste -> dernier = p_cellule;
 	} 
@@ -92,28 +110,29 @@ Cellule* ajouterElementDebut( ListeChainee* liste, Donnee elem ) {
 	}
 
 	liste -> premier = p_cellule; 	// on change la premiere cellule de la liste
-
 	liste -> nbElements++;
 
 	return p_cellule;
 }
 
-
-// ajoute un élément en fin de liste
+/** Ajoute un élément en fin de liste
+ * @param liste : la liste
+ * @param elem : l'élément
+ * @return : un pointeur de cellule
+ * */
 Cellule* ajouterElementFin( ListeChainee* liste, Donnee elem ) {
-
 	Cellule* p_cellule = (Cellule*) malloc( sizeof(Cellule) );
 
 	p_cellule -> valeur = elem;
 	p_cellule -> suivant = NULL;
 	p_cellule -> precedent = liste -> dernier;
 
-	if( ! liste -> premier ) {			// si il n'y avait pas de premier (liste vide)
+	if( ! liste -> premier ) {		// si il n'y avait pas de premier (liste vide)
 		liste -> premier = p_cellule;	// alors la derniere cellule est aussi la premiere
 	}
 
-	if( liste -> dernier ) {		// si il y avait deja une derniere cellule
-		liste -> dernier -> suivant = p_cellule;		// on dit que la cellule suivante de la derniere est ma nouvelle cellule
+	if( liste -> dernier ) {	// si il y avait deja une derniere cellule
+		liste -> dernier -> suivant = p_cellule;	// on dit que la cellule suivante de la derniere est ma nouvelle cellule
 	}
 
 	liste -> dernier = p_cellule;
@@ -123,8 +142,12 @@ Cellule* ajouterElementFin( ListeChainee* liste, Donnee elem ) {
 	return p_cellule;
 }
 
-
-// ajout un élément après une cellule identifiée par pointeur
+/** Ajoute un élément après une cellule
+ * @param liste : la liste
+ * @param cellule : la cellule après laquelle on veut insérer
+ * @param elem : l'élément
+ * @return : un pointeur de cellule
+ * */
 Cellule* ajouterElementApres( ListeChainee* liste, Cellule* cellule, Donnee elem ) {
 	if( cellule == liste -> dernier ) {
 		return ajouterElementFin( liste, elem );
@@ -144,29 +167,35 @@ Cellule* ajouterElementApres( ListeChainee* liste, Cellule* cellule, Donnee elem
 	return p_cellule;
 }
 
-
-
-// retourne un pointeur vers la première cellule de la liste (cette valeur peut être NULL si la liste est vide)
+/** Renvoi le premier élément de la liste
+ * @param liste : la liste
+ * @return : un pointeur de cellule
+ * */
 Cellule* getPremierElement( ListeChainee* liste ) {
 	return liste -> premier;
 }
 
-// retourne un pointeur vers la dernière cellule de la liste (cette valeur peut être NULL si la liste est vide)
+/** Renvoi le dernier élément de la liste
+ * @param liste : la liste
+ * @return : un pointeur de cellule
+ * */
 Cellule* getDernierElement( ListeChainee* liste ){
 	return liste -> dernier;
 }
 
-// retourne le nombre d'éléments présents dans la liste
+/** Renvoi le nombre d'éléments de la liste
+ * @param liste : la liste
+ * @return : un entier positif
+ * */
 int getNbElements( ListeChainee* liste ) {
 	return liste -> nbElements;
 }
 
-
-
-
-// détruit une cellule de la liste
+/** Détruit une cellule
+ * @param liste : la liste
+ * @param cellule : la cellule à détruire
+ * */
 void detruireCellule( ListeChainee* liste, Cellule* cellule ) {
-
 	if( cellule == liste -> premier ) liste -> premier = cellule -> suivant;
 	else cellule -> precedent -> suivant = cellule -> suivant;
 	
@@ -174,42 +203,39 @@ void detruireCellule( ListeChainee* liste, Cellule* cellule ) {
 	else cellule -> suivant -> precedent = cellule -> precedent;
 
 	free( cellule );
-
 	liste -> nbElements--;
 }
 
-
-// détruit une liste chainée (détruit également toutes les cellules qu'elle contient)
+/** Détruit une liste et toutes les cellules qu'elle contient
+ * @param liste : la liste
+ * */
 void detruireListeChainee( ListeChainee* liste ) {
-
 	Cellule* p_cellule = liste -> premier;
 	Cellule* p_aDetruire;
 
-	// on libère la mémoire allouée à chaque cellule contenu dans la liste
-	while( p_cellule ) {	// equivaux à : p_cellule != NULL
-
-		p_aDetruire = p_cellule;			
-
-		p_cellule = p_cellule -> suivant;	
-
-		free( p_aDetruire );
-
+	// on libère la mémoire allouée à chaque cellule contenue dans la liste
+	while( p_cellule ) {	// equivaut à : p_cellule != NULL
 		/*
 		pour ne pas "casser" le chainage lors de la suppression de la cellule courante,
 		on sauvegarde la cellule a détuire (cellule courante) et la cellule suivante 
 		*/
+		p_aDetruire = p_cellule;			
+		p_cellule = p_cellule -> suivant;
+		
+		free( p_aDetruire );
 	}
 
-	// on libère la mémoire allouée à la liste elle meme
+	// on libère la mémoire allouée à la liste elle-meme
 	free( liste );
 }
 
-
-
-
-// retourne dans un pointeur, la donnée maximale de la liste, on comparera les éléments de cette liste en utilisant une fonction de comparaison 
+/** Renvoi la donnée maximale contenue dans la liste
+ * @param liste : la liste
+ * @param comparaison : la fonction de comparaison à utiliser
+ * @param donneeMax : la donnée à récupérer
+ * @return : un entier
+ * */
 int getDonneeMax( ListeChainee* liste, p_fonctionComparaisonDonnee comparaison, Donnee* donneeMax ) {
-
 	int nbCoups = getNbElements(liste);
 	if( nbCoups == 0 ) {
 		printf("la liste est vide, pas de maximum \n");
@@ -219,7 +245,6 @@ int getDonneeMax( ListeChainee* liste, p_fonctionComparaisonDonnee comparaison, 
 	Cellule* cellule = getPremierElement(liste);
 	Donnee donneeMaximale = getDonnee(cellule);
 	Donnee donneeActuelle;
-
 
 	int i;
 	for( i = 1; i < nbCoups; i++ ) {
@@ -231,23 +256,18 @@ int getDonneeMax( ListeChainee* liste, p_fonctionComparaisonDonnee comparaison, 
 	}
 
 	*donneeMax = donneeMaximale;
-
 	return 0;
 }
 
-
-
-
-// applique une fonctions à tous les éléments de la liste
+/** Applique une fonction aux éléments de la liste
+ * @param liste : la liste
+ * @param fonctionElement : la fonction à appliquer
+ * */
 void appliquerFonctionSurElement( ListeChainee* liste, p_fonctionElement fonctionElement ) {
-
 	Cellule* cellule = getPremierElement(liste);
 
 	while( cellule ) {
-
 		fonctionElement( &(cellule -> valeur) );
-
 		cellule = getCelluleSuivante(cellule);
 	}
-
 }
